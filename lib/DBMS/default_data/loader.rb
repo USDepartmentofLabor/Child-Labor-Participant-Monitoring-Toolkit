88bc5987eeeaeef0528d3b43aa8b4a69d216ece3
Steven Yue <jincheker@gmail.com>
2015-06-10 16:56:57 -0400

    Change wired format.

--- lib/DBMS/default_data/loader.rb
+++ lib/DBMS/default_data/loader.rb
@@ -1,31 +1,31 @@
 module DBMS
-	module DefaultData
-		class DataAlreadyLoaded < Exception; end
+  module DefaultData
+    class DataAlreadyLoaded < Exception; end
 
-		module Loader
-			class << self
-				def no_data?
+    module Loader
+      class << self
+        def no_data?
           !WorkStatus.exists? &&
-          	!EducationStatus.exists?
+            !EducationStatus.exists?
         end
 
         def load
-        	raise DataAlreadyLoaded.new("Some configuration data is already loaded.") unless no_data?
+          raise DataAlreadyLoaded.new("Some configuration data is already loaded.") unless no_data?
 
-        	WorkStatus.create!(name: "Engaged in Child Labor")
-        	WorkStatus.create!(name: "High-Risk of Entering Child Labor")
-        	WorkStatus.create!(name: "Trafficked")
-        	WorkStatus.create!(name: "High-Risk of being Trafficked")
-        	WorkStatus.create!(name: "Commercial Sexual Exploitation")
-        	WorkStatus.create!(name: "High-Risk of Entering Commercial Sexual Exploitation")
+          WorkStatus.create!(name: "Engaged in Child Labor")
+          WorkStatus.create!(name: "High-Risk of Entering Child Labor")
+          WorkStatus.create!(name: "Trafficked")
+          WorkStatus.create!(name: "High-Risk of being Trafficked")
+          WorkStatus.create!(name: "Commercial Sexual Exploitation")
+          WorkStatus.create!(name: "High-Risk of Entering Commercial Sexual Exploitation")
 
-        	EducationStatus.create!(name: "Formal Education")
-        	EducationStatus.create!(name: "Non-formal Education")
-        	EducationStatus.create!(name: "Vocational Education")
+          EducationStatus.create!(name: "Formal Education")
+          EducationStatus.create!(name: "Non-formal Education")
+          EducationStatus.create!(name: "Vocational Education")
 
-        	true
+          true
         end
-			end	
-		end
-	end
+      end 
+    end
+  end
 end
