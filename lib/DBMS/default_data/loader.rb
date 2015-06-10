8170b6a27c60307271c47af54f1d8b7ed44ab4f3
Steven Yue <jincheker@gmail.com>
2015-06-10 15:15:28 -0400

    Create default data loader.

---
+++ lib/DBMS/default_data/loader.rb
@@ -0,0 +1,9 @@
+module DBMS
+	module DefaultData
+		class DataAlreadyLoaded < Exception; end
+
+		module Loader
+			# ref https://github.com/redmine/redmine/blob/master/lib/redmine/default_data/loader.rb
+		end
+	end
+end
