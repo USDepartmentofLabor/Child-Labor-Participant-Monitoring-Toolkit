ea710d2624bad885d8e4b05de429ce9cf303bd72
Steven Yue <syue@impaqint.com>
2015-06-12 16:51:45 -0400

    Save report to the datdatabase.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,4 +6,21 @@ class ReportsController < ApplicationController
 	def new
 		@report = Report.new
 	end
+
+  def create
+    @report = Report.new(report_params)
+    @report.user_id = current_user.id
+   if @report.save
+      redirect_to reports_path, notice: t("action_messages.create", model: "Report")
+    else
+      render :new
+    end
+  end
+
+  private
+
+    # Only allow a trusted parameter "white list" through.
+    def report_params
+      params.require(:report).permit(:title, :start_date, :end_date, :service_type, :service_ids, :target_type, :target_ids)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612133237) do
+ActiveRecord::Schema.define(version: 20150612205007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150612133237) do
     t.string   "target_type",               null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
