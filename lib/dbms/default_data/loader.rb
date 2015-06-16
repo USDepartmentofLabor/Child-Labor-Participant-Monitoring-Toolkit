0424601ff7425458beb80d67b277ddeb4b074507
Steven Yue <syue@impaqint.com>
2015-06-16 16:53:58 -0400

    Update data loader.

--- lib/dbms/default_data/loader.rb
+++ lib/dbms/default_data/loader.rb
@@ -26,7 +26,7 @@ module DBMS
           true
         end
 
-        def create_report(user)
+        def create_report(user, project)
           # create report template for education indicators
           start_date = "#{Date.today.year}-10-1"
           end_date = "#{Date.today.year+1}-3-31"
@@ -43,7 +43,8 @@ module DBMS
             service_type: "EducationStatus",
             target_ids: ["1", "2", "3", "4", "5", "6"],
             target_type: "WorkStatus",
-            user_id: user.id
+            user_id: user.id,
+            project_id: project.id
           )
 
           e11_desc = <<-eos
@@ -59,13 +60,15 @@ module DBMS
             service_type: "EducationStatus",
             target_ids: ["3", "4", "5", "6"],
             target_type: "WorkStatus",
-            user_id: user.id
+            user_id: user.id,
+            project_id: project.id
           )
         end
 
         def load_dummy_data
           load if no_data?
           user = User.create!(name: "Admin", email: "admin@impaqint.com", password: "password")
+          project = Project.create!(name: "Child Labor Example Project", user_id: user.id)
 
           index = 1
           year = Date.today.year - 13
@@ -77,24 +80,27 @@ module DBMS
           # create users with different work status
           WorkStatus.all.each do |status|
             child_num = rand(10) + 2
-            month = rand(13) + 1
+            month = rand(11) + 1
             day = rand(20) + 1
             child_num.times do
               rand_birth = Date.new(year, month, day)
               child = Child.create!(fname: "Child_#{index}", lname: "Labor", sex: index%2+1, dob: rand_birth, country: 'US')
+              project.children << child
+
               child_status = ChildStatus.create!(
                 start_date: start_date + "-#{rand(27)+1}",
                 end_date: end_date + "-#{rand(27)+1}",
                 work_status_id: status.id, 
                 education_status_id: EducationStatus.pluck(:id).sample,
                 child_id: child.id,
-                user_id: user.id
+                user_id: user.id,
+                project_id: project.id
               )
               index += 1
             end
           end
 
-          create_report(user)
+          create_report(user, project)
         end
       end 
     end
