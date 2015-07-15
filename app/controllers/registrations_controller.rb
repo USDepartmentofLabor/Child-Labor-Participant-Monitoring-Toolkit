e428c4cdc7c3b8ba352c3e919f413b23e63dbec8
Steven Yue <syue@impaqint.com>
2015-07-15 11:01:16 -0400

    Modify the sign in redirects.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,16 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def after_sign_up_path_for(resource)
+    '/dashboard'
+  end
+
+  # The path used after sign up for inactive accounts. You need to overwrite
+  # this method in your own RegistrationsController.
+  def after_inactive_sign_up_path_for(resource)
+    '/dashboard'
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:name)
   end
