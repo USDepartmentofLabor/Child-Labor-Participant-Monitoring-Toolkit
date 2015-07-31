32795009b052e8c465e0c844778a796b6c2bde2d
Steven Yue <syue@impaqint.com>
2015-07-31 13:33:31 -0400

    Update repo url.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'dbms'
-set :repo_url, 'git@gitdev:syue/DBMS.git'
+set :repo_url, 'git@gitdev.impaq.impaqinternational.com:syue/DBMS.git'
 set :deploy_via, :remote_cache
 set :branch, "master"
 # set :branch, "dark_theme"
