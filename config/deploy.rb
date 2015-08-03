46d4798e4259a4dfb964e94f61dc92bdcddb7752
Steven Yue <syue@impaqint.com>
2015-08-03 13:38:09 -0400

    Update deploy file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,9 @@ set :deploy_to, '/home/deployer/apps/dbms'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+
+# set :bundle_binstubs, nil
 
 set :sidekiq_config, '/home/deployer/apps/dbms/current/config/sidekiq.yml'
 
