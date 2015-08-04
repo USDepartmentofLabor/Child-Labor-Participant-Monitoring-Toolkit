bcc19aeeceff517c3c3a95aa41c8fffffb2bbb16
Steven Yue <syue@impaqint.com>
2015-08-04 12:59:32 -0400

    Redirect to home page if signed in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [:index]
 
   def index
+    if current_user
+      redirect_to dashboard_path
+      return
+    end
     render layout: "front"
   end
 
