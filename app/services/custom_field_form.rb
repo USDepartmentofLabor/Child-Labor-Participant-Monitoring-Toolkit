e50667cd835d15e0acdfa526858ed6eb712c43d9
Steven Yue <syue@impaqint.com>
2015-08-11 11:09:05 -0400

    Strip input.

--- app/services/custom_field_form.rb
+++ app/services/custom_field_form.rb
@@ -17,7 +17,7 @@ class CustomFieldForm
     fields.each do |field|
       next if values[field.id.to_s].blank? || values[field.id.to_s].blank?
       custom_value = CustomValue.new
-      custom_value.value_text = values[field.id.to_s]["value_text"]
+      custom_value.value_text = values[field.id.to_s]["value_text"].strip
       custom_value.custom_field_id = field.id
       custom_value.model_id = target.id
       custom_value.save
@@ -30,7 +30,7 @@ class CustomFieldForm
 
     CustomValue.where(id: ids, custom_field_id: field_ids).each do |custom_value|
       next if custom_value.value_text.to_s == values[custom_value.id.to_s]["value_text"].to_s
-      custom_value.update(value_text: values[custom_value.id.to_s]["value_text"])
+      custom_value.update(value_text: values[custom_value.id.to_s]["value_text"].strip)
     end
   end
 end
