c8682e1c79f15eddaf45612488c6913bb2ed136e
John Clements <jclements@impaqint.com>
2016-01-14 12:22:41 -0500

    Update indicators index.
    Updated the indicator's index based on
    
    https://www.pivotaltracker.com/story/show/111625796

---
+++ app/assets/javascripts/indicators.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	
+	$('#indicatorTable').DataTable({
+		//"destroy": true,
+		"paging": true,
+		"lengthChange": false,
+		"searching": true,
+		"ordering": true,
+		"info": true,
+		"autoWidth": false
+	});
+	
+});
