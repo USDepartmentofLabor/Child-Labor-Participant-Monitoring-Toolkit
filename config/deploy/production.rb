125c51cc366e6b109d98005bc98e7c5b277222cd
Tim Mathews <tmathews@impaqint.com>
2016-01-21 16:04:56 -0500

    Configure Public Key Authentication for Capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,10 @@
-set :password, ask('Server password', nil)
-
 server(
   "10.1.1.27",
   user: 'deployer',
   roles: %w{web db app worker},
   ssh_options: {
     forward_agent: true,
-    auth_methods: %w{password},
-    password: fetch(:password),
+    auth_methods: %w{publickey password},
     port: 22
   }
 )
