8a72a55bbf53251fd0cdaa63796de6f70b40e6e3
John Clements <jclements@impaqint.com>
2016-02-16 01:53:39 -0500

    Update child sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-ServiceType.create([{ name: 'Formal Educational' }, { name: 'Non-Formal Educational' }, { name: 'Vocational' }, { name: 'Other Educational' }, { name: 'Livelihood Employment' }, { name: 'Livelihood Economic Strengthening' }, { name: 'Other Livelihood' }])
-UnitOfMeasure.create([{ name: 'Numeric' }, { name: 'Percentage' }])
-EducationStatus.create([{ name: 'Formal Education'}, { name: 'Non-formal Education' },
-  { name: 'Vocational Education' }])
+ServiceType.create([
+  { name: 'Formal Educational' },
+  { name: 'Non-Formal Educational' },
+  { name: 'Vocational' },
+  { name: 'Other Educational' },
+  { name: 'Livelihood Employment' },
+  { name: 'Livelihood Economic Strengthening' },
+  { name: 'Other Livelihood' }
+])
+UnitOfMeasure.create([
+  { name: 'Numeric' },
+  { name: 'Percentage' }
+])
+EducationStatus.create([
+  { name: 'Formal Education'},
+  { name: 'Non-formal Education' },
+  { name: 'Vocational Education' }
+])
 WorkStatus.create([
   { name: 'Engaged in Child Labor', work_type: 'CL' },
   { name: 'Hazardous Child Labor', work_type: 'CL' },
