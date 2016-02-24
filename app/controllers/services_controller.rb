82700cc9cab6a71d5337d273a81bd3204516825a
Tim Mathews <tmathews@impaqint.com>
2016-02-24 16:53:23 -0500

    Remove project ID from services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,19 +1,10 @@
 class ServicesController < ApplicationController
-  before_action :set_project
   before_action :set_children
   before_action :set_service, only: [:show, :edit, :update, :destroy]
 
   # GET /services
   def index
     @services = Service.order(:name).page(params[:page])
-
-    @search = Service.ransack(params[:q])
-    @search.build_condition
-  end
-
-  def search
-    @search = Service.ransack(params[:q])
-    @services = @search.result(distinct: true)
   end
 
   # GET /services/1
@@ -34,7 +25,7 @@ class ServicesController < ApplicationController
     @service = Service.new(service_params)
 
     if @service.save
-      redirect_to services_path(), notice: t("action_messages.create", model: Service.model_name.human)
+      redirect_to services_path, notice: t("action_messages.create", model: Service.model_name.human)
     else
       render :new
     end
@@ -49,21 +40,15 @@ class ServicesController < ApplicationController
     end
   end
 
-  # only delete service-project relation. Never delete service record.
   def destroy
-    Service.where(service_id: @service.id).first.destroy
-    redirect_to services_path(), notice: t("action_messages.destroy", model: Service.model_name.human)
+    @service.destroy
+    redirect_to services_path, notice: t("action_messages.destroy", model: Service.model_name.human)
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_service
       @service = Service.find(params[:id])
-    end
-
-    def set_project
-      @project = Project.first
-      @regions = Region.all
     end
 
     def set_children
--- app/models/service.rb
+++ app/models/service.rb
@@ -3,7 +3,7 @@ class Service < ActiveRecord::Base
   has_and_belongs_to_many :children
   belongs_to :service_type
 
-  validates :name, :service_type_id, :start_date, :end_date, presence: true
+  validates :name, :service_type_id, :start_date, presence: true
 
   validate :start_date_and_end_date_must_exist_in_project_time
 
@@ -17,15 +17,15 @@ class Service < ActiveRecord::Base
       errors.add(:start_date, "can't be after project end date")
     end
 
-    if project.start_date && end_date < project.start_date
+    if project.start_date && end_date && end_date < project.start_date
       errors.add(:end_date, "can't be before project start date")
-    elsif project.end_date && end_date > project.end_date
+    elsif project.end_date && end_date && end_date > project.end_date
       errors.add(:end_date, "can't be after project end date")
     end
   end
 
   def start_date_before_end_date
-    if start_date > end_date
+    if end_date && start_date > end_date
       errors.add(:start_date, "must be before end date")
     end
   end
