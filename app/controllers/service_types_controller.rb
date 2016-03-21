f435c60552158c7117a5c9094ea51d8c5944b451
John Clements <jclements@impaqint.com>
2016-03-21 08:14:19 -0400

    Add adding and editing service types

--- app/controllers/service_types_controller.rb
+++ app/controllers/service_types_controller.rb
@@ -1,8 +1,41 @@
 class ServiceTypesController < ApplicationController
 
-  # GET /service_types
   def index
     @service_types = ServiceType.order(:name).all
+  end
+
+  def new
+    @service_type = ServiceType.new
+  end
+
+  def edit
+    @service_type = ServiceType.find(params[:id])
+  end
+
+  def create
+    @service_type = ServiceType.new(service_type_params)
+
+    if @service_type.save
+      redirect_to service_types_path, notice: t("action_messages.create", model: "Service Type")
+    else
+      render :new
+    end
+  end
+
+  def update
+    @service_type = ServiceType.find(params[:id])
+
+    if @service_type.update(service_type_params)
+      redirect_to service_types_path, notice: t("action_messages.update", model: "Service Type")
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def service_type_params
+    params.require(:service_type).permit(:name)
   end
 
 end
