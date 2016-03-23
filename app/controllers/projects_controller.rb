5c07b52c5d6cff658a1940d3bd4313b0fef1b9fc
Tim Mathews <tmathews@impaqint.com>
2016-03-23 18:33:35 -0400

    Totals should be integers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,7 +11,7 @@ class ProjectsController < ApplicationController
 
     total_children_target = @project.project_targets.find_by(project_target_type_id: 1)
     if total_children_target
-      @total_children_target = total_children_target.target
+      @total_children_target = total_children_target.target.to_i
       @total_children_percentage = (@total_children.to_f / @total_children_target.to_f) * 100.0
     else
       @total_children_target = 0
@@ -20,7 +20,7 @@ class ProjectsController < ApplicationController
 
     total_households_target = @project.project_targets.find_by(project_target_type_id: 3)
     if total_households_target
-      @total_households_target = total_households_target.target
+      @total_households_target = total_households_target.target.to_i
       @total_households_percentage = (@total_households.to_f / @total_households_target.to_f) * 100.0
     else
       @total_households_target = 0
