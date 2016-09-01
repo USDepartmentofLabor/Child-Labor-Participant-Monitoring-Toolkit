27d0b96b69db4523a32d980d4424d3c635f33672
Tim Mathews <tmathews@impaqint.com>
2016-09-01 22:25:03 +0000

    Handle Pundit not authorized exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,16 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def after_sign_in_path_for(resource)
     '/'
+  end
+
+private
+
+  def user_not_authorized
+    flash[:alert] = t "general.not_authorized"
+    redirect_to(request.referrer || root_path)
   end
 end
