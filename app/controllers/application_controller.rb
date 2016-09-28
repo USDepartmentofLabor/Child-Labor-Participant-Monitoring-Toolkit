aad3bcb0c7dca130260e94407f4bb512d9f3277a
John Clements <jclements@impaqint.com>
2016-09-28 14:29:23 -0400

    Default locale should be supplied by the client
    Added getting the locale from the client and setting it in the application
    controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  before_action :set_locale
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -18,5 +19,15 @@ private
   def user_not_authorized
     flash[:alert] = t "general.not_authorized"
     redirect_to(request.referrer || root_path)
+  end
+
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
