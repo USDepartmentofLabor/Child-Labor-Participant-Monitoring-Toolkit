199e7ade341d2ac231a9f8670eeb5f9fe51ae406
Tim Mathews <tmathews@impaqint.com>
2016-10-03 22:31:23 +0000

    Make Sidebar Collapsible
    This should make the sidebar work on mobile and support collapsing the sidebar
    to a menu of icons in a desktop view.
    
    It also highlights the current link (usually).

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,5 +1,9 @@
 module LinkHelper
   def sidebar_link (link, path, class_name = '', icon = 'fa-angle-double-right')
+    if current_page? path
+      class_name = class_name + ' active'
+    end
+
     content_tag :li, class: class_name do
       link_to path do
         content_tag(:i, '', class: "fa #{icon}") + ' ' + link
