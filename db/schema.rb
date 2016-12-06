94332eb50c9dcbc1b2023a332eb80208e2dad0e6
Tim Mathews <tmathews@impaqint.com>
2016-12-06 23:18:10 +0000

    Add columns for additional devise capabilities

--- db/schema.rb
+++ db/schema.rb
@@ -399,8 +399,13 @@ ActiveRecord::Schema.define(version: 20161206220432) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
