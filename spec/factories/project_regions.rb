eecf688454140f34169fd086e8c5448b4a274a43
Tim Mathews <tmathews@impaqint.com>
2017-01-10 16:55:54 +0000

    Remove stubbed model tests

--- spec/factories/project_regions.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :project_region do
-    country "MyString"
-state "MyString"
-project nil
-  end
-
-end
--- spec/models/child_status_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ChildStatus, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
