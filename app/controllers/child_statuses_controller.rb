0bc1826dc64bd0a34029b3f60b4bc5d3255e6298
Tim Mathews <tmathews@impaqint.com>
2017-02-08 01:06:24 +0000

    Support custom fields for child statuses

--- app/controllers/child_statuses_controller.rb
+++ app/controllers/child_statuses_controller.rb
@@ -5,17 +5,21 @@ class ChildStatusesController < ApplicationController
     @child_status = ChildStatus.new(child_status_params)
     @child_status.child_id = @child.id
     @child_status.user_id = current_user.id
+    @custom_fields = CustomField.where(model_type: "ChildFollowup")
 
     respond_to do |format|
       if @child_status.save
+        if params[:custom_fields].present? && @custom_fields.length > 0
+          CustomFieldGroup.create_or_update(@child_status, @custom_fields, params_for_custom_field)
+        end
         format.html {
           redirect_to :back, notice: t("action_messages.create", model: ChildStatus.model_name.human)
         }
         format.js
       else
         format.html {
-          redirect_to :back, alert: t("action_messages.create_failed",
-                                      model: ChildStatus.model_name.human, error: alert)
+          redirect_to :back,
+            alert: t("action_messages.create_failed", model: ChildStatus.model_name.human, error: alert)
         }
         format.js
       end
@@ -29,6 +33,11 @@ class ChildStatusesController < ApplicationController
     end
 
     def child_status_params
-      params.require(:child_status).permit(:start_date, :end_date, :work_status_id, :education_status_id, :note)
+      params.require(:child_status).permit(:start_date, :end_date, :work_status_id, :education_status_id, :note,
+        :user_id)
+    end
+
+    def params_for_custom_field
+      params.require(:custom_fields)
     end
 end
