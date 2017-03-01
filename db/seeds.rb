4d20e62d6c6534d34506af63b0b65225674f2317
Tim Mathews <tmathews@impaqint.com>
2017-03-01 23:18:58 +0000

    A little work cleaning up I18n

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,62 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-ServiceTypeCategory.create([
-  { name: 'Educational Services', definition: 'Education services refer to all formal and non-formal education services, including vocational education.' },
-  { name: 'Livelihood Services',  definition: 'Livelihood services may include, but are not limited to, the provision or linkage to education and training, employment services, economic strengthening services, consumption smoothing services, and social capital services.'}
-])
+ServiceTypeCategory.create([{
+  name: 'Educational Services',
+  definition: 'Education services refer to all formal and non-formal education services, including vocational \
+education.'
+  }, {
+  name: 'Livelihood Services',
+  definition: 'Livelihood services may include, but are not limited to, the provision or linkage to education and \
+training, employment services, economic strengthening services, consumption smoothing services, and social capital \
+services.'
+}])
 
-ServiceType.create([
-  { name: 'Education: Formal',                  definition: 'Formal education is provided and/or recognized by the government. Formal education may include government schools, private schools, religious schools, etc. Formal education services may involve the provision of goods and/or services including direct costs such as school fees and teaching and learning materials and indirect costs such as school uniforms, transportation costs, etc. These goods and/or services are intended to ensure that the child will attend and stay in school.',                                                                                                               service_type_category_id: 1 },
-  { name: 'Education: Non-Formal',              definition: 'Non-formal education is provided by any organization or body outside of the formal school system. This education may include literacy, mainstreaming education, accelerated learning, community-based education, bridge courses, remedial education, life skills, etc. Non-formal education services may lead to mainstreaming into formal education or equivalent school certificates.',                                                                                                                                                                                                                        service_type_category_id: 1 },
-  { name: 'Education: Vocational',              definition: 'Education, apprenticeships, and/or training related to a specific vocation, trade or occupation. Vocational education is differentiated from formal or non-formal education and should not be counted under formal and non-formal education services. Vocational services provided by a project should reflect a market assessment to determine appropriate skills needed for securing decent work in a given geographic area.',                                                                                                                                                                                 service_type_category_id: 1 },
-  { name: 'Livelihood: Economic Strengthening', definition: 'Economic strengthening services aim to increase the economic well-being of participants. Economic strengthening services may include the provision or linkage to micro-credit/loan programs, productivity transfers, cooperatives, and consumption smoothing services.',                                                                                                                                                                                                                                                                                                                                         service_type_category_id: 2 },
-  { name: 'Livelihood: Adult Employment',       definition: 'Employment services for adults aim to increase employment, job retention, earnings, and occupational skills of participants. Employment services may include the provision or linkage to employment assistance programs, occupational safety and health training, micro-franchise programs, job placement, apprenticeships and public works programs.',                                                                                                                                                                                                                                                          service_type_category_id: 2 },
-  { name: 'Livelihood: Other',                  definition: 'Livelihood services other than employment and economic strengthening include any project-specific interventions that do not fit under the other sub-indicators. Social capital services, for example, aim to connect a participant with networks or groups for purposes including promoting sustainable livelihoods and reducing vulnerability to child labor. Social capital services facilitate the coordination resources for the mutual benefit of the participants. Social capital services may include the provision or linkage to support groups, farmer organizations and labor sharing arrangements.',  service_type_category_id: 2 }
-])
+ServiceType.create([{
+  name: 'Education: Formal',
+  service_type_category_id: 1,
+  definition: 'Formal education is provided and/or recognized by the government. Formal education may include \
+government schools, private schools, religious schools, etc. Formal education services may involve the provision of \
+goods and/or services including direct costs such as school fees and teaching and learning materials and indirect \
+costs such as school uniforms, transportation costs, etc. These goods and/or services are intended to ensure that the \
+child will attend and stay in school.'
+  }, {
+  name: 'Education: Non-Formal',
+  service_type_category_id: 1,
+  definition: 'Non-formal education is provided by any organization or body outside of the formal school system. This \
+education may include literacy, mainstreaming education, accelerated learning, community-based education, bridge \
+courses, remedial education, life skills, etc. Non-formal education services may lead to mainstreaming into formal \
+education or equivalent school certificates.'
+  }, {
+  name: 'Education: Vocational',
+  service_type_category_id: 1,
+  definition: 'Education, apprenticeships, and/or training related to a specific vocation, trade or occupation. \
+Vocational education is differentiated from formal or non-formal education and should not be counted under formal and \
+non-formal education services. Vocational services provided by a project should reflect a market assessment to \
+determine appropriate skills needed for securing decent work in a given geographic area.'
+  }, {
+  name: 'Livelihood: Economic Strengthening',
+  service_type_category_id: 2,
+  definition: 'Economic strengthening services aim to increase the economic well-being of participants. Economic \
+strengthening services may include the provision or linkage to micro-credit/loan programs, productivity transfers, \
+cooperatives, and consumption smoothing services.'
+  }, {
+  name: 'Livelihood: Adult Employment',
+  service_type_category_id: 2,
+  definition: 'Employment services for adults aim to increase employment, job retention, earnings, and occupational \
+skills of participants. Employment services may include the provision or linkage to employment assistance programs, \
+occupational safety and health training, micro-franchise programs, job placement, apprenticeships and public works \
+programs.',
+  }, {
+  name: 'Livelihood: Other',
+  service_type_category_id: 2,
+  definition: 'Livelihood services other than employment and economic strengthening include any project-specific \
+interventions that do not fit under the other sub-indicators. Social capital services, for example, aim to connect a \
+participant with networks or groups for purposes including promoting sustainable livelihoods and reducing \
+vulnerability to child labor.  Social capital services facilitate the coordination resources for the mutual benefit \
+of the participants. Social capital services may include the provision or linkage to support groups, farmer \
+organizations and labor sharing arrangements.'
+}])
 
 UnitOfMeasure.create([
   { name: 'Numeric' },
@@ -25,16 +68,16 @@ UnitOfMeasure.create([
 ])
 
 EducationStatus.create([
-  { name: 'Formal Education'},
+  { name: 'Formal Education' },
   { name: 'Non-formal Education' },
   { name: 'Vocational Education' }
 ])
 
 WorkStatus.create([
-  { name: 'Engaged in Child Labor',             work_type: 'CL' },
-  { name: 'Hazardous Child Labor',              work_type: 'CL' },
-  { name: 'Worst Form of Child Labor (WFCL)',   work_type: 'CL' },
-  { name: 'High Risk of Entering Child Labor',  work_type: 'CACHR' }
+  { name: 'Engaged in Child Labor',            work_type: 'CL' },
+  { name: 'Hazardous Child Labor',             work_type: 'CL' },
+  { name: 'Worst Form of Child Labor (WFCL)',  work_type: 'CL' },
+  { name: 'High Risk of Entering Child Labor', work_type: 'CACHR' }
 ])
 
 ProjectTargetType.create([
@@ -62,18 +105,6 @@ ReportingStatus.create([
   { name: 'Ready for Revision' },
   { name: 'In Revision' },
   { name: 'Approved' }
-])
-
-ReportingPeriod.create([
-  { start_date: '2015-10-01', end_date: '2016-03-31', name: 'PERIOD 1 : 1 Oct. 2015 - 31 Mar. 2016'},
-  { start_date: '2016-04-01', end_date: '2016-09-30', name: 'PERIOD 2 : 1 Apr. 2016 - 30 Sept. 2016'},
-  { start_date: '2016-10-01', end_date: '2017-03-31', name: 'PERIOD 3 : 1 Oct. 2016 - 31 Mar. 2017'},
-  { start_date: '2017-04-01', end_date: '2017-09-30', name: 'PERIOD 4 : 1 Apr. 2017 - 30 Sept. 2017'},
-  { start_date: '2017-10-01', end_date: '2018-03-31', name: 'PERIOD 5 : 1 Oct. 2017 - 31 Mar. 2018'},
-  { start_date: '2018-04-01', end_date: '2018-09-30', name: 'PERIOD 6 : 1 Apr. 2018 - 30 Sept. 2018'},
-  { start_date: '2018-10-01', end_date: '2019-03-31', name: 'PERIOD 7 : 1 Oct. 2018 - 31 Mar. 2019'},
-  { start_date: '2019-04-01', end_date: '2019-09-30', name: 'PERIOD 8 : 1 Apr. 2019 - 30 Sept. 2019'},
-  { start_date: '2019-10-01', end_date: '2020-03-31', name: 'PERIOD 9 : 1 Oct. 2019 - 31 Mar. 2020'},
 ])
 
 Ability.create([
@@ -133,33 +164,30 @@ Frequency.create ([
   { code: 'B',  name: 'Biennial' }
 ])
 
-IntakeActorType.create([
-  { name: 'Reviewer' },
-  { name: 'Supervisor' }
-])
-
 Locale.create([
   { code: 'en', name: 'English' },
   { code: 'es', name: 'Spanish' }
 ])
 
 Translation.create([
-  { locale: 'en', key: 'login.banner', value: 'Direct Beneficiary Monitoring System' },
   { locale: 'en', key: 'general.logo_alt', value: 'Department of Labor Seal' },
-  { locale: 'en', key: 'login.instruction', value: 'Login with your email address and password' },
+  { locale: 'en', key: 'general.submit', value: 'Submit' },
+  { locale: 'en', key: 'login.banner', value: 'Direct Beneficiary Monitoring System' },
   { locale: 'en', key: 'login.email', value: 'Email Address' },
+  { locale: 'en', key: 'login.forgot_password', value: 'Forgot your password?' },
+  { locale: 'en', key: 'login.instruction', value: 'Login with your email address and password' },
   { locale: 'en', key: 'login.password', value: 'Password' },
   { locale: 'en', key: 'login.remember_me', value: 'Remember me' },
-  { locale: 'en', key: 'general.submit', value: 'Submit' },
-  { locale: 'en', key: 'login.forgot_password', value: 'Forgot your password?' },
+  { locale: 'en', key: 'login.tagline', value: 'The more we connect, the better it gets.' },
 
-  { locale: 'es', key: 'login.banner', value: 'Direct Beneficiary Monitoring System' },
-  { locale: 'es', key: 'login.tagline', value: 'Cuanto más nos conectamos, mejor se pone.' },
   { locale: 'es', key: 'general.logo_alt', value: 'Sello del Departamento de Trabajo' },
-  { locale: 'es', key: 'login.instruction', value: 'Iniciar sesión con su dirección de correo electrónico y contraseña' },
+  { locale: 'es', key: 'general.submit', value: 'Presentar' },
+  { locale: 'es', key: 'login.banner', value: 'Direct Beneficiary Monitoring System' },
   { locale: 'es', key: 'login.email', value: 'Dirección de correo electrónico' },
+  { locale: 'es', key: 'login.forgot_password', value: '¿Olvidaste tu contraseña?' },
+  { locale: 'es', key: 'login.instruction',
+    value: 'Iniciar sesión con su dirección de correo electrónico y contraseña' },
   { locale: 'es', key: 'login.password', value: 'Contraseña' },
   { locale: 'es', key: 'login.remember_me', value: 'Recuérdame' },
-  { locale: 'es', key: 'general.submit', value: 'Presentar' },
-  { locale: 'es', key: 'login.forgot_password', value: '¿Olvidaste tu contraseña?' }
+  { locale: 'es', key: 'login.tagline', value: 'Cuanto más nos conectamos, mejor se pone.' }
 ])
