0ba58d92ef33e52785792bdb05de79e6207dfd4e
Tim Mathews <tmathews@impaqint.com>
2017-04-13 20:35:00 +0000

    Working tests for children index view

--- spec/views/children/index.html.erb_spec.rb
+++ spec/views/children/index.html.erb_spec.rb
@@ -1,40 +1,21 @@
 require 'rails_helper'
+require 'spec_helper'
+require 'pp'
 
-RSpec.describe "children/index", type: :view do
+describe "children/index" do
   before(:each) do
-    assign(:children, [
-      Child.create!(
-        :fname => "Fname",
-        :lname => "Lname",
-        :mname => "Mname",
-        :sex => 1,
-        :address => "Address",
-        :city => "City",
-        :state => "State",
-        :country => "Country"
-      ),
-      Child.create!(
-        :fname => "Fname",
-        :lname => "Lname",
-        :mname => "Mname",
-        :sex => 1,
-        :address => "Address",
-        :city => "City",
-        :state => "State",
-        :country => "Country"
-      )
-    ])
+    @children =
+      create_list(:child, 2, first_name: 'Fname', last_name: 'Lname', sex: 1,
+                  age: 11, intake_date: '2017-01-01')
   end
 
   it "renders a list of children" do
     render
-    assert_select "tr>td", :text => "Fname".to_s, :count => 2
-    assert_select "tr>td", :text => "Lname".to_s, :count => 2
-    assert_select "tr>td", :text => "Mname".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => "Address".to_s, :count => 2
-    assert_select "tr>td", :text => "City".to_s, :count => 2
-    assert_select "tr>td", :text => "State".to_s, :count => 2
-    assert_select "tr>td", :text => "Country".to_s, :count => 2
+
+    expect(rendered).to have_selector("tr>td", :text => "Fname", :count => 2)
+    expect(rendered).to have_selector("tr>td", :text => "Lname", :count => 2)
+    expect(rendered).to have_selector("tr>td", :text => "Boy", :count => 2)
+    expect(rendered).to have_selector("tr>td", :text => "11", :count => 2)
+    expect(rendered).to have_selector("tr>td", :text => "2017-01-01", :count => 2)
   end
 end
