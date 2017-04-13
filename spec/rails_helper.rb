dba6f73a53fb633b931dcc60b3dd2b18d0765baf
Tim Mathews <tmathews@impaqint.com>
2017-04-13 19:34:42 +0000

    Add rspec configuration to use fuubar and guard

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
+require 'support/controller_macros'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'devise'
 
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -67,4 +68,5 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.extend ControllerMacros, :type => :controller
 end
