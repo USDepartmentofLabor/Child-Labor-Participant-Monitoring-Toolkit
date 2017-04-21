96929fb8c911867f9da7998d85cb5593f1a3a913
Tim Mathews <tmathews@impaqint.com>
2017-04-21 20:43:02 +0000

    Remove unused code from CustomSection model

--- app/models/custom_section.rb
+++ app/models/custom_section.rb
@@ -1,13 +1,3 @@
 class CustomSection < ActiveRecord::Base
   has_many :custom_fields
-  belongs_to :sectionable, polymorphic: true
-
-  def custom_fields_with_values model_id
-    cf = []
-
-    custom_fields.each do |c|
-      c
-    end
-  end
-
 end
