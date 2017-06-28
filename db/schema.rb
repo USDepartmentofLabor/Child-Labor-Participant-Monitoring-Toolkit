4f10d80dbce6a29ced9f2fd59fad235422e4df6f
Tim Mathews <tmathews@impaqint.com>
2017-06-28 10:17:39 -0400

    Add currency field to income sources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522224632) do
+ActiveRecord::Schema.define(version: 20170628141049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -170,6 +170,7 @@ ActiveRecord::Schema.define(version: 20170522224632) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.text     "unit_of_measure"
+    t.string   "currency"
   end
 
   add_index "income_sources", ["household_id"], name: "index_income_sources_on_household_id", using: :btree
