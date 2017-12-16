9253b9c6562031b316b632cb6ee2f141b56c40f5
Tim Mathews <tim@signalk.org>
2017-12-16 18:18:59 -0500

    TPR submission with attachments and comments

--- app/workers/submit_tpr_worker.rb
+++ app/workers/submit_tpr_worker.rb
@@ -5,45 +5,52 @@ class SubmitTprWorker
 
     puts "Submitting TPR #{id}"
 
-    tpr = Report.find(id)
+    input = Report.find(id)
+    output = SwaggerClient::Report.new
 
-    output = SwaggerClient::TprUpload.new
-
-    output.id = tpr.id
-    output.reporting_period_end = tpr.reporting_period.end_date
-    output.submitter = {
-      name: tpr.submitted_by
+    output.id = input.id
+    output.reporting_period = {
+      start_date: input.reporting_period.start_date,
+      end_date: input.reporting_period.end_date,
+      period_type: 'standard'
     }
-    output.submitted_on = tpr.submitted_on
-    output.is_final_report = tpr.is_final_report
-
+    output.submitted_by = {
+      # TODO
+      name: 'Submitter',
+      email: 'submitter@example.com'
+    }
+    output.submitted_on = Time.now
+    #output.is_final_report = input.is_final_report
     output.attachments = []
-
-    tpr.report_attachments.each do |att|
-      annex = att.attachment_annex
-      version = att.attachment_version
-      body = Base64.encode64(File.read(att.attachment.path))
-
-      output.attachments.push(
-        {
-          annex: annex,
-          body: body,
-          revision: version
-        }
-      )
+    input.report_attachments.each do |att|
+      output.attachments.push(SwaggerClient::Attachment.new(
+        annex: att.attachment_annex,
+        version: att.attachment_version,
+        body: Base64.encode64(File.read(att.attachment.path)),
+        file_name: att.attachment_file_name,
+        mime_type: att.attachment_content_type
+      ))
+    end
+    output.comments = []
+    input.comments.each do |c|
+      output.comments.push(SwaggerClient::Comment.new(
+        author: c.author,
+        body: c.body,
+        submitted_on: c.submitted_at
+      ))
     end
 
     # Only queued TPRs can be submitted
-    if tpr.report_status_id != 6
+    if input.report_status_id != 6
       return
     end
 
-    client = SwaggerClient::DefaultApi.new
+    client = SwaggerClient::ReportApi.new
 
     begin
-      client.upload_tpr(tpr)
-      tpr.report_status_id = 2
-      tpr.save
+      client.add_report output
+#      tpr.report_status_id = 2
+#      tpr.save
     rescue SwaggerClient::ApiError => e
       puts "Exception when calling API->submit_tpr: #{e}"
     end
