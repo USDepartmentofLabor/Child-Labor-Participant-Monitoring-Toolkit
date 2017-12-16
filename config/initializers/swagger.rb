aa332dc4b9ba33ad4e58d17264da9562f1203d45
Tim Mathews <tim@signalk.org>
2017-12-16 18:18:38 -0500

    HACK: Hard coded API key. FIXME

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,5 +1,5 @@
 SwaggerClient.configure do |c|
-  c.api_key['Authorization'] = Project.first.api_key
+  c.api_key['Authorization'] = 'Token token="f4c6ee360846549f07e5155ebfaec5db"' #Project.first.api_key
   c.scheme = ENV['API_SCHEME'] || 'http'
   c.host = ENV['API_HOST']
   c.base_path = '/api'
