c657c80cc119bd46f11c055303e938dc552073dc
Nicholas Schoeb <nschoeb@impaqint.com>
2017-12-27 11:40:19 -0500

    add update method to household api

--- app/controllers/api/v1/households_controller.rb
+++ app/controllers/api/v1/households_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::HouseholdsController < Api::ApiController
+  before_action :set_household, only: [:update]
 
   def index
     render json: Household.all.to_json
@@ -7,13 +8,26 @@ class Api::V1::HouseholdsController < Api::ApiController
   def create
     @household = Household.new(household_params)
     if @household.save
-      render json: '{"status":"submitted"}'
+      render json: '{"status":"household create successful"}'
     else
-      render json: '{"status":"failed"}'
+      render json: '{"status":"household create failed"}'
     end       
+  end
+  
+  def update   
+    if @household.update(household_params)
+      render json: '{"status":"update successful"}'
+    else
+      render json: '{"status":"update failed"}'
+    end
   end
 
   private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_household
+      @household = Household.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def household_params
