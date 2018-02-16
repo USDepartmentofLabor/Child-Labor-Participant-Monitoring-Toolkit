fa0fedbd234b90e73a0ec3a37ce215a98637aeeb
Nicholas Schoeb <nschoeb@impaqint.com>
2018-02-16 08:40:05 -0500

    follow ups api status codes and error handling

--- app/controllers/api/v1/follow_ups_controller.rb
+++ app/controllers/api/v1/follow_ups_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::FollowUpsController < Api::ApiController
   before_filter :ensure_user_allowed
-  before_action :set_follow_up, only: [:show, :update]
 
-  # GET /api/v1/follow_ups
+  # GET /api/follow_ups
   def index
     if FollowUp.count.zero?
       render json: '[]'
@@ -17,36 +16,72 @@ class Api::V1::FollowUpsController < Api::ApiController
     end
   end
 
-  # GET /api/v1/follow_ups/1
+  # GET /api/follow_ups/1
   def show
-    render json: get_follow_up_as_json(@follow_up)
+    if FollowUp.exists?(params[:id])
+      set_follow_up
+      render json: get_follow_up_as_json(@follow_up)
+    else
+      render status: 404, json: '{"status":"failure"}'
+    end
   end
 
   # POST /api/v1/follow_ups
   def create
+    if !validate_request
+      render status: 400, json: '{"status":"failure"}'
+      return
+    end
+
     @follow_up = FollowUp.new(follow_up_params)
-    if @follow_up.save            
-      render json: '{"status":"success","updated_at":"' +
-        get_datetime_formatted(@follow_up.updated_at) +
-        '","created_at":"' +
-        get_datetime_formatted(@follow_up.created_at) +
-        '","id":"' + @follow_up.id.to_s + '"}'
+    if @follow_up.save
+      render status: 201, json: '{"status":"success","updated_at":"' +
+        get_datetime_formatted(@follow_up.updated_at) + '","created_at":"' +
+        get_datetime_formatted(@follow_up.created_at) + '","id":"' +
+        @follow_up.id.to_s + '"}'
     else
-      render json: '{"status":"failure"}'
-    end      
+      render status: 500, json: '{"status":"failure"}'
+    end
   end
 
-  # PUT /api/v1/people/1
-  def update   
-    if @follow_up.update(follow_up_params)
-      render json: '{"status":"success", "updated_at":"' +
-        get_datetime_formatted(@follow_up.updated_at) + '"}'
+  # PUT /api/follow_ups/1
+  def update
+    if FollowUp.exists?(params[:id])
+      set_follow_up
+      if !validate_request
+        render status: 400, json: '{"status":"failure"}'
+        return
+      end
+      if @follow_up.update(follow_up_params)
+        render json: '{"status":"success", "updated_at":"' +
+          get_datetime_formatted(@follow_up.updated_at) + '"}'
+      else
+        render status: 500, json: '{"status":"failure"}'
+      end
     else
-      render json: '{"status":"failure"}'
+      render status: 404, json: '{"status":"failure"}'
     end
   end
 
   private
+
+  def validate_request
+    # check if person_id is valid if not null
+    if follow_up_params[:person_id].present?
+      if !Person.exists?(follow_up_params[:person_id])
+        return false
+      end
+    end
+
+    # do not allow null person_id
+    if follow_up_params[:person_id].blank?
+      return false
+    end
+
+    # TODO: ids x 3
+
+    return true
+  end
 
   def set_follow_up
     @follow_up = FollowUp.find(params[:id])
@@ -62,12 +97,12 @@ class Api::V1::FollowUpsController < Api::ApiController
   # Never trust parameters from the scary internet, only allow the white list through.
   def follow_up_params
     params.require(:follow_up).permit(
-      :person_id,
-      :follow_date, :did_work, :have_job_returning_to, :hours_worked, :occupation_id, :industry_id,
-      :exposed_to_hazardous_condition, :subject_to_abuses, :performed_housework,
-      :hours_worked_on_housework, :enrolled_in_school, :latitude, :longitude,
-      :position_accuracy, :altitude, :altitude_accuracy, :heading, :speed,
-      :gps_recorded_at,
+      :person_id, :follow_date, :did_work, :have_job_returning_to,
+      :hours_worked, :occupation_id, :industry_id,
+      :exposed_to_hazardous_condition, :subject_to_abuses,
+      :performed_housework, :hours_worked_on_housework, :enrolled_in_school,
+      :latitude, :longitude, :position_accuracy, :altitude, :altitude_accuracy,
+      :heading, :speed, :gps_recorded_at,
       :work_activity_ids => [],
       :hazardous_condition_ids => [],
       :household_task_ids => [])
