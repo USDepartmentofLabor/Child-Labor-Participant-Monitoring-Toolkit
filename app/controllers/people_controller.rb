af632e0bc11eed9964b63149ae38f0135ca474a2
John Clements <jclements@impaqint.com>
2018-04-11 13:53:37 -0400

    Add custom fields to the person intake form
    ref #96

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,6 +3,13 @@ class PeopleController < ApplicationController
   before_action :set_person, only: [:show, :edit, :update, :destroy]
 
   def show
+    if ! CustomSection.where(model_type: 'Person', sort_order: 1).empty?
+      @section_1_custom_fields = CustomSection.where(model_type: 'Person', sort_order: 1).first.custom_fields
+    else
+      @section_1_custom_fields = Array.new
+    end
+    @sections = CustomSection.where('model_type = ? AND sort_order > ?', 'Person', 1)
+
     @person_statuses = Array.new
     intake_work_status_name = @person.intake_work_status.name unless @person.intake_work_status.nil?
     intake_education_status_name = @person.intake_education_status.name unless @person.intake_education_status.nil?
@@ -31,15 +38,22 @@ class PeopleController < ApplicationController
   def new
     @person = Person.new
     @person.household = @household
+    @section_1_custom_fields = CustomSection.where(model_type: 'Person', sort_order: 1).first.custom_fields unless CustomSection.where(model_type: 'Person', sort_order: 1).empty?
+    @sections = CustomSection.where('model_type = ? AND sort_order > ?', 'Person', 1)
   end
 
   def create
     @person = Person.new(person_params)
+    @custom_fields = CustomField.where(model_type: "Person")
 
     @person.household_id = @household.id
     @person.age = Time.now.year - @person.dob.year
 
     if @person.save
+      if params[:custom_fields].present? && @custom_fields.length > 0
+        CustomFieldGroup.create_or_update(@person, @custom_fields, params_for_custom_field)
+      end
+
       redirect_to household_path(@household), notice: t("action_messages.create", model: Person.model_name.human)
     else
       render :new
@@ -48,11 +62,18 @@ class PeopleController < ApplicationController
   end
 
   def edit
+    @section_1_custom_fields = CustomSection.where(model_type: 'Person', sort_order: 1).first.custom_fields unless CustomSection.where(model_type: 'Person', sort_order: 1).empty?
+    @sections = CustomSection.where('model_type = ? AND sort_order > ?', 'Person', 1)
   end
 
   def update
+    @custom_fields = CustomField.where(model_type: "Person")
 
     if @person.update_attributes(person_params)
+      if params[:custom_fields].present? && @custom_fields.length > 0
+        CustomFieldGroup.create_or_update(@person, @custom_fields, params_for_custom_field)
+      end
+
       @person.age = Time.now.year - @person.dob.year
       @person.save
       redirect_to household_person_path(@household, @person), notice: t("action_messages.update", model: Person.model_name.human)
@@ -90,6 +111,10 @@ class PeopleController < ApplicationController
       :hazardous_condition_ids => [],
       :household_task_ids => []
     )
+  end
+
+  def params_for_custom_field
+    params.require(:custom_fields)
   end
 
 end
